84c991fc2b2db585b5cdba30114080ac3a105e1f
ghalidouga <ghalighalidouga@gmail.com>
2020-01-05 22:57:46 +0700

    damage sharing implemented

--- src/main/java/Events/TakeDamage.java
+++ src/main/java/Events/TakeDamage.java
@@ -1,13 +1,29 @@
 package Events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+
+import java.util.Collection;
 
 public class TakeDamage implements Listener {
     @EventHandler
     public void takeDamage (EntityDamageEvent e){
         EntityType player = e.getEntityType();
+        double finalDamage = e.getFinalDamage();
+        for (Player p : Bukkit.getServer().getOnlinePlayers()){
+            if(player.equals(EntityType.PLAYER)){
+                double health = p.getHealth();
+                p.setHealth(health-finalDamage);
+
+
+            }
+        }
+
     }
 }
--- src/main/java/ghaliwang/sharepain/SharePain.java
+++ src/main/java/ghaliwang/sharepain/SharePain.java
@@ -1,5 +1,6 @@
 package ghaliwang.sharepain;
 
+import Events.TakeDamage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SharePain extends JavaPlugin {
@@ -7,6 +8,7 @@ public final class SharePain extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(new TakeDamage(),this);
 
     }
 
