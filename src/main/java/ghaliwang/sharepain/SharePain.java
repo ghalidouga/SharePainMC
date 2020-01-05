1da4efc95c1afa1e1479861a5bc63750a3bd4a1b
ghalidouga <ghalighalidouga@gmail.com>
2020-01-05 21:27:30 +0700

    first commit

---
+++ src/main/java/ghaliwang/sharepain/SharePain.java
@@ -0,0 +1,17 @@
+package ghaliwang.sharepain;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SharePain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
